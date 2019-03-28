1cc069deefe3a75c3c02466e3c5903a15bf3cddb
doanhlam <30718414+doanhlam@users.noreply.github.com>
2019-03-28 09:31:55 +0700

    Load maze

--- app_class.py
+++ app_class.py
@@ -11,12 +11,18 @@ class App:
         self.clock = pygame.time.Clock()
         self.running = True
         self.state ='start'
+
+        self.load()   #Load board
     def run(self):
         while self.running:
             if self.state == 'start':
                 self.state_events()
                 self.state_update()
                 self.state_draw()
+            if self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
@@ -26,9 +32,13 @@ class App:
         font = pygame.font.SysFont(font_name,size,)
         text = font.render(words, False,color)
         text_size = text.get_size()
-        pos[0] = pos[0]-text_size[0]//2
-        pos[1] = pos[1] - text_size[1] // 2
+        if center:
+            pos[0] = pos[0]-text_size[0]//2
+            pos[1] = pos[1] - text_size[1] // 2
         screen.blit(text,pos)
+    def load(self):
+        self.background = pygame.image.load('board.png')
+        self.background = pygame.transform.scale(self.background,(WIDTH,HEIGHT)) ###Scale the image
 #######################Intro##################################
 
     def state_events(self):
@@ -43,6 +53,25 @@ class App:
         pass
     def state_draw(self):
         self.sceen.fill(BLACK)  #Background color
+        #######Menu#####
         self.draw_text('Press Space Bar',self.sceen,[WIDTH//2,HEIGHT//2],
-                        START_TEXT_SIZE,(170,132,58),START_FONT)
+                        START_TEXT_SIZE,(170,132,58),START_FONT,center= True)
+        self.draw_text('1 Player', self.sceen, [WIDTH // 2, HEIGHT // 2+35],
+                       START_TEXT_SIZE, (33,137,156), START_FONT,center= True)
+        self.draw_text('High Score', self.sceen, [0,0],
+                       START_TEXT_SIZE, (255, 255, 255), START_FONT)
+        pygame.display.update()
+
+######################Play###############
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+
+    def playing_update(self):
+        pass
+    def playing_draw(self):
+        self.sceen.blit(self.background,(0,0)) #Game color
         pygame.display.update()
--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 #SCREEN
 FPS = 60
-WIDTH, HEIGHT = 500,700
+WIDTH, HEIGHT = 560,620
 #color,player,ghost setting
 BLACK = (0,0,0)
+RED = (208,22,22)
 START_TEXT_SIZE = 16
 
 START_FONT ='arial black'
